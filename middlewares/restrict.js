9df4da6285d31779788311e34eb2bbd9fc1fc069
adamwisnup <wisnuadam21@gmail.com>
2024-10-02 22:06:40 +0700

    feat: add product features

--- middlewares/restrict.js
+++ middlewares/restrict.js
@@ -1,8 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-// const { JWT_SECRET_KEY } = process.env;
-
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
 
@@ -18,18 +16,18 @@ module.exports = async (req, res, next) => {
   const token = authorization.split(" ")[1];
 
   // Log token untuk memverifikasi apakah token sudah benar
-  console.log("Token received:", token);
+  // console.log("Token received:", token);
 
   // Verifikasi token menggunakan secret key
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
     // Log secret key untuk memastikan nilainya diatur dengan benar
-    console.log("JWT_SECRET_KEY:", process.env.JWT_SECRET_KEY);
+    // console.log("JWT_SECRET_KEY:", process.env.JWT_SECRET_KEY);
 
     // Log decoded token untuk melihat hasilnya
-    console.log("Decoded token:", decoded);
+    // console.log("Decoded token:", decoded);
 
     if (err) {
-      console.log("Error verifying token:", err); // Log error jika terjadi masalah saat verifikasi
+      console.log("Error verifying token:", err);
       return res.status(401).json({
         status: false,
         message: "Failed to authenticate token",
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const restrict = require("../middlewares/restrict");
 const { image } = require("../libs/multer");
 const UserController = require("../features/users/controllers/user");
+const ProductController = require("../features/products/controllers/product");
 
 // TESTING
 router.get("/users", UserController.getAllUser);
@@ -20,5 +21,15 @@ router.patch(
   UserController.updateAvatar
 );
 router.patch("/profile", restrict, UserController.updateProfile);
+
+// PRODUCT
+router.get("/products", restrict, ProductController.getAllProducts);
+router.get("/products/:id", restrict, ProductController.getProductById);
+router.get(
+  "/products/category/:category",
+  restrict,
+  ProductController.getProductByCategory
+);
+router.delete("/products/:id", restrict, ProductController.deleteProduct);
 
 module.exports = router;
