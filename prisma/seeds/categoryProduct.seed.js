5299d8faa2d675f727934481c1997697ba49276b
adamwisnup <wisnuadam21@gmail.com>
2025-04-23 10:00:16 +0700

    fix: update category product seed data to correct food and beverage classifications

--- prisma/seeds/categoryProduct.seed.js
+++ prisma/seeds/categoryProduct.seed.js
@@ -5,58 +5,73 @@ const seedCategoryProducts = async () => {
     data: [
       {
         id: 1,
-        name: "Kopi",
-        type: "BEVERAGE",
-      },
-      {
-        id: 2,
-        name: "Teh",
-        type: "BEVERAGE",
-      },
-      {
-        id: 3,
-        name: "Isotonik",
-        type: "BEVERAGE",
-      },
-      {
-        id: 4,
-        name: "Jus",
-        type: "BEVERAGE",
-      },
-      {
-        id: 5,
-        name: "Air Mineral",
-        type: "BEVERAGE",
-      },
-      {
-        id: 6,
-        name: "Susu",
-        type: "BEVERAGE",
-      },
-      {
-        id: 7,
         name: "Roti",
         type: "FOOD",
       },
       {
-        id: 8,
-        name: "Mie",
-        type: "FOOD",
-      },
-      {
-        id: 9,
-        name: "Frozen Food",
-        type: "FOOD",
-      },
-      {
-        id: 10,
+        id: 2,
         name: "Wafer & Biskuit",
         type: "FOOD",
       },
       {
-        id: 11,
+        id: 3,
+        name: "Frozen Food",
+        type: "FOOD",
+      },
+      {
+        id: 4,
+        name: "Mie",
+        type: "FOOD",
+      },
+      {
+        id: 5,
         name: "Snack",
         type: "FOOD",
+      },
+      {
+        id: 6,
+        name: "Sereal",
+        type: "FOOD",
+      },
+      {
+        id: 7,
+        name: "Soda",
+        type: "BEVERAGE",
+      },
+      {
+        id: 8,
+        name: "Kopi",
+        type: "BEVERAGE",
+      },
+      {
+        id: 9,
+        name: "Yogurt",
+        type: "BEVERAGE",
+      },
+      {
+        id: 10,
+        name: "Susu",
+        type: "BEVERAGE",
+      },
+      {
+        id: 11,
+        name: "Jus",
+        type: "BEVERAGE",
+      },
+      {
+        id: 12,
+        name: "Isotonik",
+        type: "BEVERAGE",
+      },
+      {
+        id: 13,
+        name: "Air Mineral",
+        type: "BEVERAGE",
+      },
+      {
+        id: 14,
+        name: "Teh",
+        type: "BEVERAGE",
       },
     ],
   });
